be046b629688d26cfb99a2b432fe661c9cd58453
shelby palumbo <shelbypalumbo94@gmail.com>
2020-02-27 20:31:09 -0500

    initial file commits

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+
