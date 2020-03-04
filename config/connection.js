ef177186d70a482a0eb32f8be677afe201a91fd4
shelby palumbo <shelbypalumbo94@gmail.com>
2020-03-03 20:41:06 -0500

    hide password

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
-var password = require("password");
+var password = require("./password");
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
